42f50478a3d94fe5906c3b6ace1b59c674e00328
reham <najehreham9@gmail.com>
2025-09-20 00:51:13 +0300

    modify custom_text_field.dart to be suitable for all textfields

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:krakebic/widgets/custom_text_field.dart';
-import 'package:krakebic/widgets/password_field.dart';
 // import 'package:krakebic/constants.dart';
 
 class LoginPage extends StatelessWidget {
@@ -16,32 +15,36 @@ class LoginPage extends StatelessWidget {
         child: Center(
           child: Padding(
             padding: const EdgeInsets.all(18.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Image.asset('assets/images/logo.png'),
+            child: SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Image.asset('assets/images/logo.png'),
 
-                Text('Log In', style: Theme.of(context).textTheme.titleLarge),
-                CustomTextField(
-                  lable: 'Email',
-                  hintText: 'Enter your email address',
-                  controller: emailControlller,
-                ),
-                SizedBox(height: 16),
-                PasswordField(
-                  lable: "Password",
-                  hintText: 'Enter your emai password',
-                ),
-                SizedBox(height: 61),
-                SizedBox(
-                  width: double.infinity,
-                  height: 44,
-                  child: ElevatedButton(
-                    onPressed: () {},
-                    child: Text('Log in'),
+                  Text('Log In', style: Theme.of(context).textTheme.titleLarge),
+                  CustomTextField(
+                    lable: 'Email',
+                    hintText: 'Enter your email address',
+                    controller: emailControlller,
                   ),
-                ),
-              ],
+                  SizedBox(height: 16),
+
+                  CustomTextField(
+                    lable: "Password",
+                    hintText: 'Enter your password',
+                    isPassword: true,
+                  ),
+                  SizedBox(height: 61),
+                  SizedBox(
+                    width: double.infinity,
+                    height: 44,
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      child: Text('Log in'),
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
--- lib/widgets/custom_text_field.dart
+++ lib/widgets/custom_text_field.dart
@@ -1,46 +1,76 @@
 import 'package:flutter/material.dart';
 
-class CustomTextField extends StatelessWidget {
+class CustomTextField extends StatefulWidget {
   const CustomTextField({
     super.key,
     required this.lable,
     required this.hintText,
     this.controller,
     this.validator,
+    this.isPassword = false,
   });
   final String lable;
   final String hintText;
   final FormFieldValidator<String>? validator;
   final TextEditingController? controller;
+  final bool isPassword;
+
+  @override
+  State<CustomTextField> createState() => _CustomTextFieldState();
+}
+
+class _CustomTextFieldState extends State<CustomTextField> {
+  bool _obscure = true;
+
+  @override
+  void initState() {
+    super.initState();
+    _obscure = widget.isPassword;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
 
       children: [
-        Text(
-          lable,
-          style: Theme.of(
-            context,
-          ).textTheme.labelMedium,
-        ),
+        Text(widget.lable, style: Theme.of(context).textTheme.labelMedium),
         SizedBox(height: 8),
         TextFormField(
-          
-         // style: Theme.of(context).textTheme.displayMedium,
-          controller: controller,
-
+          // style: Theme.of(context).textTheme.displayMedium,
+          controller: widget.controller,
+          validator: widget.validator,
+          obscureText: widget.isPassword ? _obscure : false,
+          obscuringCharacter: '*',
           decoration: InputDecoration(
             enabledBorder: OutlineInputBorder(
               borderRadius: BorderRadius.circular(16),
-              borderSide: BorderSide(color: Color(0xffEDEDED))
-              ),
-            hintText: hintText,
+              borderSide: BorderSide(color: Color(0xffEDEDED)),
+            ),
+            focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(16),
+              borderSide: const BorderSide(color: Color(0xff009639)),
+            ),
+            hintText: widget.hintText,
             hintStyle: Theme.of(context).textTheme.bodySmall,
             filled: true,
             fillColor: Color(0xffF6F6F6),
+              suffixIcon: widget.isPassword
+                ? IconButton(
+                    icon: Icon(
+                      _obscure ? Icons.visibility_off : Icons.visibility,
+                      color: Colors.grey,
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        _obscure = !_obscure;
+                      });
+                    },
+                  )
+                : null,
           ),
-          validator: validator,
+          
+          
         ),
       ],
     );
